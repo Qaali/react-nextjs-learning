605a2d86a9fb012e66be63adf5902d85cd84ab7b
Qaali <qali@ualberta.ca>
2025-09-07 19:11:36 -0600

    Initial React and Next.js app setup

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div className="font-sans flex items-center justify-center min-h-screen p-8">
+      <main className="flex flex-col gap-6 items-center text-center">
+        <h1 className="text-2xl font-bold">
+          Hello, my name is Qamar.
+        </h1>
+        <p className="text-lg">
+          My first React and Next.JS page.
+        </p>
+        <p className="text-lg">
+          More to come :)
+        </p>
+      </main>
+    </div>
+  );
+}
